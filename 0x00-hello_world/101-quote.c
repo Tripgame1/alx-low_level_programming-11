ae576c832c10d848b8ab478ff08c57ad1101d4a9
Tripgame1 <thatsmemohamed@gmail.com>
2023-03-16 12:04:52 -0700

    Task Advanced 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the amin function
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	fputs("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
