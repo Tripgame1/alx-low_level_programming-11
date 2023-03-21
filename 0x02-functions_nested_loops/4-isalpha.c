a9f79ca61ce6c0e014cb3c3c662497eb72b627a9
Tripgame1 <thatsmemohamed@gmail.com>
2023-03-21 11:21:06 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - shows 1 if the input is a
+ * letter another cases, shows 0
+ *
+ * @c: letter
+ *
+ * Return: 1 for letters. 0 for the rest
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+		else
+		{
+			return (0);
+		}
+	_putchar('\n');
+}
