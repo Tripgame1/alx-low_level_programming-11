042e4ae2346a5eecb420f25f497550ecab11ea3b
Tripgame1 <thatsmemohamed@gmail.com>
2023-03-21 09:42:00 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet (void) - prints the lowercase alphabets from a to z
+ * print_alphabet - prints the alphabet in lowercase.
  * Return: always 0
  */
 
