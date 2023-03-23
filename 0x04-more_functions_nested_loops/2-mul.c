082f1789d0660e9c96d732411221826fe49b60d6
Tripgame1 <thatsmemohamed@gmail.com>
2023-03-23 06:40:18 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - command for multimlication
+ *
+ *@a: input number 1
+ *@b: input number 2
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
